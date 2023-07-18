6654f15038eba5b0bf7d1a1c9d9a953f22d9a592
tjdqja0806 <74092254+tjdqja0806@users.noreply.github.com>
2023-07-18 10:26:17 +0900

    First Upload
    Upload All

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; //static 사용으로 인해 유일성을 보장
+    public static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 가지고 온다
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
